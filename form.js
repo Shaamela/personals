ea525dd8e1188601a4cba0b0e6f8be0b5ef02f26
shaamela <hashimx46@gmail.com>
2026-01-02 16:22:08 +0100

    form added

---
+++ form.js
@@ -0,0 +1,39 @@
+
+let errorMessage = document.querySelector('#errorMessage');
+const form = document.querySelector('#form');
+const date = document.querySelector('#start');
+const textArea = document.querySelector('textarea');
+form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const {firstName, email, phone, start, end } = form.elements;
+        errorMessage.textContent = "";
+        if (!firstName.value.trim()) {
+                displayError("Name is required");
+                firstName.focus()
+                return
+        }
+        let value = start.valueAsDate;
+        let date = new Date();
+        if (value  <= date) {
+                displayError('Enter a later date, please!');
+                date.focus()
+                return
+        }
+        if (textArea.value.length < 50) {
+                displayError('Enter a message of fifty minimum words. Pls include your location')
+                textArea.focus();
+                return
+        }
+
+        form.elements.firstName.value = "";
+        form.elements.email.value = "";
+        form.elements.phone.value = "";
+        form.elements.start.value ="";
+        form.elements.end.value = "";
+        textArea.value = "",
+        
+
+});
+function displayError(error) {
+        errorMessage.textContent = `${error}`;
+}
